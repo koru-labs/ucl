f1b19c03f671f34f6dadcb1adc1287892d7d7269
Angel Valkov <eternaltimeparadox@gmail.com>
2026-03-09 10:59:38 +0200

    Initialize logger

--- jsonrpc/filter_manager.go
+++ jsonrpc/filter_manager.go
@@ -451,6 +451,7 @@ func (f *FilterManager) NewLogFilter(logQuery *LogQuery, ws wsConn) string {
 	filter := &logFilter{
 		filterBase: newFilterBase(ws),
 		query:      logQuery,
+		logs:       []*Log{},
 	}
 
 	if filter.hasWSConn() {
